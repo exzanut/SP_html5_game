738e5268d893b035ae5c6391843f988af3c44120
Lunak <najezenejjezek@gmail.com>
2013-04-23 18:06:58 +0200

    uprava enemy a shoot
    pridan enemy spawner

--- www/script/enemy.js
+++ www/script/enemy.js
@@ -1,11 +1,41 @@
+var EnemySpawner = enchant.Class.create(enchant.Sprite,{
+    initialize: function () {
+      var game = Game.instance;
+      Sprite.call(this,0,0);
+         this.addEventListener('enterframe', function () {
+            if(game.frame%60==0) {
+              var x = Math.random()*game.width;
+              var enemy;
+            if(Math.random()*2<1){
+              enemy = new Bear(x, 0);
+            }else{
+              enemy = new Bear3(x, 0);
+            } 
+            enemy.key = game.frame;
+            //game.enemies[game.frame] = enemy;
+            game.scGame.addChild(enemy);   
+          }
+          if(game.frame%40==0) {
+            var x = Math.random()*game.width;
+            var enemy = new Bear2(x, 0); 
+            enemy.key = game.frame;
+            //game.enemies[game.frame] = enemy;
+            game.scGame.addChild(enemy);
+          }
+          });
+       
+    } 
+});
+
+
+
 var Enemy = enchant.Class.create(enchant.Sprite, {
 
     initialize: function (x, y,mArray) {
         /**
          * As with the Player class, you set size at 16x16 Sprite base and expand.
          */
-        enchant.Sprite.call(this, 32, 32);
-        Game.instance.assets['enemy.png'];
+        enchant.Sprite.call(this, 16, 16);
         this.x = x;
         this.y = y;
         
@@ -13,7 +43,8 @@ var Enemy = enchant.Class.create(enchant.Sprite, {
         moveArray=mArray;
 
          this.addEventListener('enterframe', function () {
-            if(this.y > 700 || this.x > 320 || this.x < -this.width || this.y < -this.height) {
+            if(this.y > Game.instance.height || this.x > Game.instance.width || this.x < -this.width || this.y < -this.height) {
+              console.log("mazu");
                 this.remove();
             }
 
@@ -27,10 +58,13 @@ var Enemy = enchant.Class.create(enchant.Sprite, {
             
             this.move(); 
             if(this.age%20==0){
-              var shoot=new EnemyShoot(this.x,this.y+10,5/4*Math.PI);
-              var shoot=new EnemyShoot(this.x,this.y+10,7/4*Math.PI);
+              var shoot=new EnemyShoot(this.x,this.y+10,3/2*Math.PI);
+              //var shoot=new EnemyShoot(this.x,this.y+10,5/4*Math.PI);
+              //var shoot=new EnemyShoot(this.x,this.y+10,7/4*Math.PI);
             } 
         });
+
+
     },
     /**
      * Sets function called up for movement.
@@ -44,8 +78,8 @@ var Enemy = enchant.Class.create(enchant.Sprite, {
     },
     remove: function () {
         
-        Game.instance.scene.removeChild(this);
-        delete enemies[this.key];
+        Game.instance.scGame.removeChild(this);
+       // delete game.enemies[this.key];
     }
 });        
 
@@ -54,13 +88,15 @@ var Bear = enchant.Class.create(Enemy, {
 
    initialize: function (x, y) {
        var moveArray = new Array(); //[age,angle,speed]
-       if(x>250){
+       if(x>Game.instance.width/2){
          moveArray=[[0,270,4],[40,135,3],[60,270,5],[90,210,7]];
        }else{
          moveArray=[[0,270,4],[40,45,3],[60,270,5],[90,330,7]];
        }
        Enemy.call(this, x, y,moveArray);
-       this.frame = 3;
+       Game.instance.assets['www/picture/enemy.png'];
+       this.frame = 22;
+
     }
 });
 
@@ -69,13 +105,14 @@ var Bear2 = enchant.Class.create(Enemy, {
 
    initialize: function (x, y) {
        var moveArray = new Array();
-       if(x>250){
+       if(x>Game.instance.width/2){
         moveArray=[[0,270,4],[20,210,9],[50,270,7]];
        }else{
         moveArray=[[0,270,4],[20,330,9],[50,270,7]]; 
        }
        Enemy.call(this, x, y,moveArray);
-       this.frame = 4;
+       this.image = Game.instance.assets['www/picture/enemy.png'];
+       this.frame = 11;
     }
 });
 
@@ -85,6 +122,7 @@ var Bear3 = enchant.Class.create(Enemy, {
        var moveArray = new Array();  
        moveArray=[[0,270,3],[15,250,4],[35,290,4],[55,250,4],[75,290,4],[90,250,4]];
        Enemy.call(this, x, y,moveArray);
-       this.frame = 5;
+       this.image = Game.instance.assets['www/picture/enemy.png'];
+       this.frame = 23;
     }
 });
