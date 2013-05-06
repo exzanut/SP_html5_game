3d9671f2dc99e6d402b9e0ed29e0444bd68b1053
Vítězslav Kačmár <vitezslav.kacmar@seznam.cz>
2013-05-06 13:46:19 +0200

    pridana reakce  poskozeni lodi na strely, opraven bug sirky aktivni herni plochy pri vytvareni nove hry, pridan efekt zniceni strely a lodi, pridany zvuky strely, lode, pozadi, pridano tlacitko ovladani zvuku do menu

--- www/script/shoot.js
+++ www/script/shoot.js
@@ -11,7 +11,7 @@ var Shoot = enchant.Class.create(enchant.Sprite, {
         this.addEventListener('enterframe', function () {
             this.x += this.moveSpeed * Math.cos(this.direction);
             this.y -= this.moveSpeed * Math.sin(this.direction);
-            if(this.y > game.height || this.x > game.width || this.x < -this.width || this.y < -this.height) {
+            if(this.y > game.height || this.x > game.gameW-this.width || this.x < -this.width || this.y < -this.height) {
                 this.remove();
             }
         });
@@ -30,13 +30,14 @@ var EnemyShoot = enchant.Class.create(Shoot, {
         this.moveSpeed = 10;
         this.image = Game.instance.assets['www/picture/enemy.png'];
         this.frame = 60;
-        this.damage=1;
+        this.damage=2;
         
-       /* this.addEventListener('enterframe', function () {
-            if(player.within(this, 8)) {
-                game.end(game.score, "SCORE: " + game.score)
+       this.addEventListener('enterframe', function () {
+            if(Game.instance.playerShip.intersect(this)) {
+                this.remove();
+                Game.instance.playerShip.getDmg(this.damage);
             }
-        });*/
+        });
     }
 });
 
@@ -47,34 +48,51 @@ var RoundEnShoot = enchant.Class.create(EnemyShoot, {
         this.image = Game.instance.assets['www/picture/ball.png'];
         this.frame = 4;
         this.damage=1;
-        
-       
     }
 });
 
-
 var PlayerShoot = enchant.Class.create(Shoot, {
-
     initialize: function (x, y,direction) {
-        var game = Game.instance;
         Shoot.call(this, x, y,direction);
-        this.moveSpeed = 20;
-
-        this.image = game.assets['www/picture/ball.png'];
-        this.sound = game.assets['www/sound/M4A1_Single.wav'];
-        this.sound.clone().play();
-        this.frame = 7;
-        this.damage = 1;
 
         this.addEventListener('enterframe', function () {
             for(var i =0;i<Game.instance.enemies.length;i++){
                 var enemy = Game.instance.enemies[i];
                 if (this.intersect(enemy)){
-                    this.remove();
+                    this.destroyShoot();
                     enemy.getDmg(this.damage);
                 }
             }
         });
+    }
+});
 
+var BasePlayerShoot = enchant.Class.create(PlayerShoot, {
+    initialize: function (x, y,direction) {
+        PlayerShoot.call(this, x, y,direction);
+        var game = Game.instance;
+        this.moveSpeed = 20;
+        this.image = game.assets['www/picture/ball.png'];
+        this.sound = game.assets['www/sound/baseShoot.wav'];
+        if(Game.instance.soundTurn == true) this.sound.clone().play();
+        this.frame = 7;
+        this.damage = 1;
+    },
+
+    destroyShoot: function () {
+        this.image = Game.instance.assets['www/picture/shoot_effect.png'];
+        this.sound = this.sound = Game.instance.assets['www/sound/baseExplosion.wav'];
+        if(Game.instance.soundTurn == true) this.sound.clone().play();
+        this.frame = 0;
+        this.moveSpeed = 0;
+
+        this.addEventListener('enterframe', function () {
+            if(Game.instance.fps%2 ==0){
+                this.frame++;
+                if(this.frame == 5){
+                    this.remove();
+                }
+            }
+        });
     }
 });
